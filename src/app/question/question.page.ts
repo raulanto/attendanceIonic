99a41a37b600282bc4940666cd3d0f355653dbaa
StrHiugh <116786621+StrHiugh@users.noreply.github.com>
2023-10-05 00:12:49 -0600

    init views

---
+++ src/app/question/question.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+import axios from 'axios';
+
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.page.html',
+  styleUrls: ['./question.page.scss'],
+})
+export class QuestionPage implements OnInit {
+  questions: any = [];
+  constructor(
+    private loadingCtrl: LoadingController,
+
+  ) { }
+
+  ngOnInit() {
+    this.cargarQuestions();
+
+  }
+
+  async cargarQuestions(event?: InfiniteScrollCustomEvent) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+    const response = await axios({
+      method: 'GET',
+      url: "http://attendancedb1.test/question?expand=tag,teacher,person",
+      withCredentials: true,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then((response) => {
+      this.questions = response.data;
+      event?.target.complete();
+    }).catch(function (error) {
+      console.log(error);
+    });
+    loading.dismiss();
+  }
+
+
+}
