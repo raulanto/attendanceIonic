d8fde77a1dba829cb575ad5a3ed8f8dceabf0f49
raul <74162376+raulanto@users.noreply.github.com>
2023-10-05 09:19:16 -0600

    page-Preguntas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  {
+    path: 'question',
+    loadChildren: () => import('./question/question.module').then(m => m.QuestionPageModule)
+  },
 ];
 @NgModule({
   imports: [
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
 }
