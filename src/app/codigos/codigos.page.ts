6457a3cdf3531a88a7d0d6b5d47789f35d6168d0
raul <74162376+raulanto@users.noreply.github.com>
2023-10-09 14:09:03 -0600

    Codigo vista bien

--- src/app/codigos/codigos.page.ts
+++ src/app/codigos/codigos.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+import axios from 'axios';
 
 @Component({
   selector: 'app-codigos',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CodigosPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	constructor(
+		private route: ActivatedRoute,
+		private loadingCtrl : LoadingController,
+		private loading: LoadingController
+	) { }
+	codigos: any = [];
+	ngOnInit() {
+		this.cargarCodigo()
+	}
+	async cargarCodigo(event?: InfiniteScrollCustomEvent) {
+		const loading = await this.loadingCtrl.create({
+			message : 'Cargando',
+			spinner : 'bubbles',
+		});
+		await loading.present();
+		const response = await axios({
+			method: 'get',
+			url : "http://attendancedb.test/code",
+			withCredentials: true,
+			headers: {
+				'Accept': 'application/json'
+			}
+		}).then( (response) => {
+			this.codigos = response.data;
+			event?.target.complete();
+		}).catch(function (error) {
+			console.log(error);
+		});
+		loading.dismiss();
+	}
+	
 
 }
