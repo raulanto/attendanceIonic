48123d8057815edb13cb8bbc48ff5e6002293b73
raul <74162376+raulanto@users.noreply.github.com>
2023-10-09 13:46:55 -0600

    cambios

--- src/app/lista-codigos/lista-codigos.page.ts
+++ src/app/lista-codigos/lista-codigos.page.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+import axios from 'axios';
 @Component({
-  selector: 'app-lista-codigos',
-  templateUrl: './lista-codigos.page.html',
-  styleUrls: ['./lista-codigos.page.scss'],
+	selector: 'app-lista-codigos',
+	templateUrl: './lista-codigos.page.html',
+	styleUrls: ['./lista-codigos.page.scss'],
 })
 export class ListaCodigosPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	constructor(
+		private route: ActivatedRoute,
+		private loadingCtrl : LoadingController,
+		private loading: LoadingController
+	) { }
+	codigos: any = [];
+	ngOnInit() {
+		this.cargarCodigo()
+	}
+	async cargarCodigo(event?: InfiniteScrollCustomEvent) {
+		const loading = await this.loadingCtrl.create({
+			message : 'Cargando',
+			spinner : 'bubbles',
+		});
+		await loading.present();
+		const response = await axios({
+			method: 'get',
+			url : "http://attendancedb.test/code",
+			withCredentials: true,
+			headers: {
+				'Accept': 'application/json'
+			}
+		}).then( (response) => {
+			this.codigos = response.data;
+			event?.target.complete();
+		}).catch(function (error) {
+			console.log(error);
+		});
+		loading.dismiss();
+	}
+	
 
 }
