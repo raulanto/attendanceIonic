6db387b21eed2379b86729e7fc96c54c5ef88469
raul <74162376+raulanto@users.noreply.github.com>
2023-10-09 22:07:43 -0600

    sistema

--- src/app/lista-asistencia/lista-asistencia.page.ts
+++ src/app/lista-asistencia/lista-asistencia.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+import axios from 'axios';
 
 @Component({
   selector: 'app-lista-asistencia',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAsistenciaPage implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private route: ActivatedRoute,
+    private loadingCtrl: LoadingController,
+    private loading: LoadingController
+  ) { }
+  asistencia: any = [];
   ngOnInit() {
+    this.cargarAsistencia()
+  }
+  async cargarAsistencia(event?: InfiniteScrollCustomEvent) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+    const response = await axios({
+      method: 'get',
+      url: "http://attendancedb.test/attendance",
+      withCredentials: true,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then((response) => {
+      this.asistencia = response.data;
+      event?.target.complete();
+    }).catch(function (error) {
+      console.log(error);
+    });
+    loading.dismiss();
   }
 
 }
