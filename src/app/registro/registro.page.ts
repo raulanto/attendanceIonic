e5e4911606f585ef8a590f6bc2398e84329e4482
raul <74162376+raulanto@users.noreply.github.com>
2023-11-28 23:13:24 -0600

    registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  tipoPersona: string='';
+  
 
   constructor() { }
-
+  //Cargar tipo de estudio
+  degrees:any=[];
+  //base de degree
+  baseDegre:string='http://attendancedb.test/degree';
   ngOnInit() {
+    this.cargarDegree();
   }
 
+
+  customActionSheetOptions = {
+    header: 'Que eres?',
+    subHeader: 'Seleciona tu posicion en tu institucion',
+  };
+
+  async cargarDegree() {
+    const response = await axios({
+      method: 'get',
+      url: this.baseDegre,
+      withCredentials: true,
+      headers: {
+        //token Bearer 100-token
+        'Accept': 'application/json',
+        'Authorization': 'Bearer 100-token'
+      }
+    }).then((response) => {
+      this.degrees = response.data;
+      // console.log(this.degrees);
+      
+    }).catch(function (error) {
+      console.log(error);
+    });
+    
+  }
 }
