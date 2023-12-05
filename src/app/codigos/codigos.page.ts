dd710bf329fab43d00fb78fdc13c05c621374071
raul <74162376+raulanto@users.noreply.github.com>
2023-12-04 22:58:18 -0600

    ultimo cambios

--- src/app/codigos/codigos.page.ts
+++ src/app/codigos/codigos.page.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { InfiniteScrollCustomEvent, LoadingController, AlertController,ModalController } from '@ionic/angular';
+import {
+	InfiniteScrollCustomEvent,
+	LoadingController,
+	AlertController,
+	ModalController,
+} from '@ionic/angular';
 import axios from 'axios';
 import { GenerarCodigoPage } from '../generar-codigo/generar-codigo.page';
+import { CodigoService } from '../services/codigo.service';
 
 @Component({
 	selector: 'app-codigos',
@@ -16,63 +22,87 @@ export class CodigosPage implements OnInit {
 		private route: ActivatedRoute,
 		private loadingCtrl: LoadingController,
 		public modalCtrl: ModalController,
-		private alertController: AlertController
+		private alertController: AlertController,
+		private codigoService: CodigoService
 	) {
 		this.grupoid = this.route.snapshot.paramMap.get('grupoid');
-
 	}
-	codigos: any = [];
+	data: any = [];
 	ngOnInit() {
 		this.cargarCodigo();
 		this.mostrar();
 	}
 
-
+	// async cargarCodigo(event?: InfiniteScrollCustomEvent) {
+	// 	const loading = await this.loadingCtrl.create({
+	// 		message: 'Cargando',
+	// 		spinner: 'bubbles',
+	// 	});
+	// 	await loading.present();
+	// 	const response = await axios({
+	// 		method: 'get',
+	// 		url: "http://attendance.test/code/codigos?id=" + this.grupoid,
+	// 		withCredentials: true,
+	// 		headers: {
+	// 			'Accept': 'application/json',
+	// 			//token Bearer 100-token
+	// 			'Authorization': 'Bearer 100-token'
+	// 		}
+	// 	}).then((response) => {
+	// 		this.codigos = response.data;
+	// 		event?.target.complete();
+	// 	}).catch(function (error) {
+	// 		console.log(error);
+	// 	});
+	// 	loading.dismiss();
+	// }
 
 	async cargarCodigo(event?: InfiniteScrollCustomEvent) {
-		const loading = await this.loadingCtrl.create({
-			message: 'Cargando',
-			spinner: 'bubbles',
-		});
-		await loading.present();
-		const response = await axios({
-			method: 'get',
-			url: "http://attendance.test/code/codigos?id=" + this.grupoid,
-			withCredentials: true,
-			headers: {
-				'Accept': 'application/json',
-				//token Bearer 100-token
-				'Authorization': 'Bearer 100-token'
-			}
-		}).then((response) => {
-			this.codigos = response.data;
-			event?.target.complete();
-		}).catch(function (error) {
+		try {
+			const loading = await this.loadingCtrl.create({
+				message: 'Cargando',
+				spinner: 'bubbles',
+			});
+			await loading.present();
+			await this.codigoService.codigos(this.grupoid).subscribe(
+				async (response) => {
+					if (response?.status == 200) {
+						this.data = response.data;
+						event?.target.complete();
+						console.log('coorecto');
+					} else if (response?.status === 400) {
+						console.log(response?.error);
+					}
+				},
+				(error) => {
+					if (error.status == 422) {
+						console.log(error);
+					}
+				}
+			);
+		} catch (error) {
 			console.log(error);
-		});
-		loading.dismiss();
+		}
 	}
 
 	mostrar() {
 		console.log('Valor de grupoid en codigo:', this.grupoid);
 	}
 	async new() {
-		// Crear una página modal utilizando el controlador de modales 
+		// Crear una página modal utilizando el controlador de modales
 		const paginaModal = await this.modalCtrl.create({
-		  component: GenerarCodigoPage, // El componente que se mostrará en el modal
-		  componentProps: {
-			parametro: this.grupoid, // Puedes enviar cualquier tipo de dato
-			// Agrega más parámetros según sea necesario
-		  },
-		  breakpoints: [0, 0.3, 0.5, 0.95], // Configuración de puntos de quiebre
-		  initialBreakpoint: 0.95, // Ubicación inicial del punto de quiebre
+			component: GenerarCodigoPage, // El componente que se mostrará en el modal
+			componentProps: {
+				parametro: this.grupoid, // Puedes enviar cualquier tipo de dato
+				// Agrega más parámetros según sea necesario
+			},
+			breakpoints: [0, 0.3, 0.5, 0.95], // Configuración de puntos de quiebre
+			initialBreakpoint: 0.95, // Ubicación inicial del punto de quiebre
 		});
-	  
+
 		// Presentar la página modal en la interfaz de usuario
 		await paginaModal.present();
-	  }
-	  
-
+	}
 
 	async showDataAlert(codigo: any) {
 		const alert = await this.alertController.create({
@@ -82,15 +112,9 @@ export class CodigosPage implements OnInit {
 			Hora: ${codigo.cod_time}<br>
 			Fecha: ${codigo.cod_date}<br>
 			Duración: ${codigo.cod_duration}`,
-			buttons: ['OK']
+			buttons: ['OK'],
 		});
 
 		await alert.present();
 	}
-
-
-
-
-
-
 }
--- src/app/services/codigo.service.ts
+++ src/app/services/codigo.service.ts
@@ -7,7 +7,7 @@ import axios from 'axios';
 })
 export class CodigoService {
   urlCode:string  = `${environment.apiUrl}code/`;
-  headers:any = {'Content-Type': 'application/json'}; 
+  headers:any = {'Content-Type': 'application/json', 'Authorization': 'Bearer '+localStorage.getItem('token') || 'Bearer 100-token'};
   constructor() { }
 
   generarCodigo(codigo:any):Observable<any>{
@@ -27,5 +27,24 @@ export class CodigoService {
         });
     });
   }
+  
+  codigos(id:number): Observable<any> {
+    const url = `${this.urlCode}codigos?id=`+id;
+    return new Observable(observer => {
+      axios.get(url, {
+        withCredentials: true,
+        headers: this.headers
+      })
+      .then(response => {
+        observer.next(response.data);
+        observer.complete();
+      })
+      .catch(error => {
+        observer.error(error);
+        observer.complete();
+      });
+    });
+  }
+
   
 }
