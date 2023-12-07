371d495bed7c4b2bc06bc1f754aeba4af5096cb7
StrHiugh <116786621+StrHiugh@users.noreply.github.com>
2023-12-07 02:09:13 -0600

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,17 +8,22 @@ const routes: Routes = [
   },
   {
     path: 'menu1/:gro_id/:gro_subject/:gro_code',
-    loadChildren: () => import('./menu1/menu1.module').then( m => m.Menu1PageModule)
+    loadChildren: () => import('./menu1/menu1.module').then(m => m.Menu1PageModule)
   },
   {
     path: 'question',
     loadChildren: () => import('./question/question.module').then(m => m.QuestionPageModule)
-  },  {
+  },
+  {
     path: 'new-question',
-    loadChildren: () => import('./new-question/new-question.module').then( m => m.NewQuestionPageModule)
+    loadChildren: () => import('./new-question/new-question.module').then(m => m.NewQuestionPageModule)
+  },
+  {
+    path: 'question-detail/:que_id',
+    loadChildren: () => import('./question-detail/question-detail.module').then(m => m.QuestionDetailPageModule)
   },
 
-  
+
 ];
 @NgModule({
   imports: [
@@ -26,4 +31,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
