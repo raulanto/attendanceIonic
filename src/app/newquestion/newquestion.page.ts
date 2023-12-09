b0f3938e9da0591f2f2ba183add4fa6a96f5d7b6
MoniCamacho <MoniGpe.0@gmail.com>
2023-12-09 15:03:40 -0600

    Avances vista maestro

--- src/app/newquestion/newquestion.page.ts
+++ src/app/newquestion/newquestion.page.ts
@@ -9,8 +9,8 @@ import { AlertController, IonicModule, ModalController } from '@ionic/angular';
   styleUrls: ['./newquestion.page.scss'],
 })
 export class NewquestionPage implements OnInit {
-  questionUrl: string = "http://attendancedb1.test/question" 
-  tagUrl: string = "http://attendancedb1.test/tag/"
+  questionUrl: string = "http://attendance.test/question" 
+  tagUrl: string = "http://attendance.test/tag/"
   @Input() id: number | undefined; //id traido desde el otro ts
   public question!: FormGroup; //aqui 
   tags: any = [];
